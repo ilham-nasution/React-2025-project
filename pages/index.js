ff9a6cf9bf53c825f31ca1c2a1945eb8dc7afa95
Ilham Rahman <ilhaamrahman@gmail.com>
2020-12-22 12:19:06 +0700

    add chakra ui and custom fonts

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,31 @@
+import { Button, Code, Heading, Text } from "@chakra-ui/react";
 import Head from "next/head";
+
 import { useAuth } from "../lib/auth";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>React 2025 Project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          <code className={styles.code}>{auth?.user?.email}</code>
-        </p>
+      <main>
+        <Heading>React 2025 Project</Heading>
+        <Text>
+          <Code>{auth?.user?.email}</Code>
+        </Text>
         {auth.user ? (
-          <button onClick={() => auth.signOut()}>Sign out</button>
+          <Button onClick={() => auth.signOut()}>Sign out</Button>
         ) : (
-          <button onClick={() => auth.signInWithGitHub()}>
+          <Button onClick={() => auth.signInWithGitHub()}>
             Sign in with github
-          </button>
+          </Button>
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
