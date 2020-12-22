4e8ff595cff2fd725f13a4254af581054b18861a
Ilham Rahman <ilhaamrahman@gmail.com>
2020-12-22 14:34:33 +0700

    create dashboard and input for add a site

---
+++ components/DashboardShell.js
@@ -0,0 +1,76 @@
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Heading,
+  Button,
+  Flex,
+  Link,
+  Avatar,
+  Icon,
+} from "@chakra-ui/react";
+
+import { useAuth } from "@/lib/auth";
+
+const DashboardShell = ({ children }) => {
+  const { user, signOut } = useAuth();
+
+  return (
+    <Box backgroundColor="gray.100" h="100vh">
+      <Flex backgroundColor="white" mb={16} w="full">
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          px={8}
+          py={4}
+          maxW="1250px"
+          margin="0 auto"
+          w="full"
+        >
+          <Flex>
+            <Icon viewBox="0 0 46 32" boxSize={8} mr={8}>
+              <path
+                d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61 9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418 7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
+                fill="currentColor"
+              />
+            </Icon>
+            <Link mr={4}>Sites</Link>
+            <Link>Feedback</Link>
+          </Flex>
+          <Flex justifyContent="center" alignItems="center">
+            <Button variant="ghost" mr={2} onClick={() => signOut()}>
+              Log Out
+            </Button>
+            <Avatar size="sm" src={user.photoURL} />
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex margin="0 auto" direction="column" maxW="1250px" px={8}>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbLink>Sites</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <Flex justifyContent="space-between">
+          <Heading mb={8}>My Sites</Heading>
+          <Button
+            backgroundColor="gray.900"
+            color="white"
+            fontWeight="medium"
+            _hover={{ bg: "gray.700" }}
+            _active={{
+              bg: "gray.800",
+              transform: "scale(0.95)",
+            }}
+          >
+            + Add Site
+          </Button>
+        </Flex>
+        {children}
+      </Flex>
+    </Box>
+  );
+};
+
+export default DashboardShell;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,40 @@
-import { Button, Code, Heading, Text } from "@chakra-ui/react";
 import Head from "next/head";
+import { Button, Flex, Icon } from "@chakra-ui/react";
 
 import { useAuth } from "@/lib/auth";
 
-export default function Home() {
+const Home = () => {
   const auth = useAuth();
 
   return (
-    <>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
-        <title>React 2025 Project</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Fast Feedback</title>
       </Head>
 
-      <main>
-        <Heading>React 2025 Project</Heading>
-        <Text>
-          <Code>{auth?.user?.email}</Code>
-        </Text>
-        {auth.user ? (
-          <Button onClick={() => auth.signOut()}>Sign out</Button>
-        ) : (
-          <Button onClick={() => auth.signInWithGitHub()}>
-            Sign in with github
-          </Button>
-        )}
-      </main>
-    </>
+      <Icon viewBox="0 0 46 32" boxSize={8}>
+        <path
+          d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61 9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418 7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
+          fill="currentColor"
+        />
+      </Icon>
+      {auth.user ? (
+        <Button as="a" href="/dashboard">
+          View Dashboard
+        </Button>
+      ) : (
+        <Button mt={4} size="sm" onClick={() => auth.signInWithGitHub()}>
+          Sign In
+        </Button>
+      )}
+    </Flex>
   );
-}
+};
+
+export default Home;
